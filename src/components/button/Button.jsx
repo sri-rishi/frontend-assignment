5f24794c79a844badc0405c5e9e8da76d8ee4847
sri-rishi <sririshi2612@gmail.com>
2024-02-01 12:13:12 +0530

    feat: create Button component

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,8 @@
+const Button = ({className, text, icon}) => {
+    return (
+        <button className={className && className}>{icon && icon}{text && text}</button>
+    )
+}
+
+
+export default Button;
