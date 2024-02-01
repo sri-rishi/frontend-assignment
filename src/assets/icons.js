61bae4cb0374280c2ad5c96b982e697d4aabe22c
sri-rishi <sririshi2612@gmail.com>
2024-02-01 20:10:01 +0530

    added assets

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,4 +1,8 @@
-import { IoSearch } from "react-icons/io5";
+import { IoSearch, IoHeart,IoCaretDownOutline } from "react-icons/io5";
+import { BiFilterAlt } from "react-icons/bi";
+import { IoIosTrendingUp } from "react-icons/io";
+import { GoSponsorTiers } from "react-icons/go";
 
 
-export {IoSearch}
+
+export {IoSearch, BiFilterAlt, IoIosTrendingUp, GoSponsorTiers, IoHeart,IoCaretDownOutline}
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,40 @@
 import LOGO from "./images/bandiya_logo.png";
 import APP_BG from "./images/app-bg.png";
-import {IoSearch} from "./icons";
+import NEWEST_CHIP from "./images/newest-chip.png";
+import INDIAN_FLAG from "./images/indian-flag.png";
+import SEAL_OF_TRUST from "./images/seal-of-trust.png";
+import USER_1 from "./images/I1.png";
+import USER_2 from "./images/I2.png";
+import USER_3 from "./images/I3.png";
+import BALL from "./images/ball.png";
+import HAND from "./images/hand.png";
+import CAR from "./images/car.png";
+import SPACE from "./images/space-suit.png";
+import BIKE from "./images/bike.png";
+import ROBOTICS from "./images/robotics.jpg";
+import PLAYING_BUTTON from "./images/playing-button.png";
+import {IoSearch, BiFilterAlt,IoIosTrendingUp, GoSponsorTiers, IoHeart, IoCaretDownOutline} from "./icons";
 
 export {
     LOGO,
     APP_BG,
-    IoSearch
+    NEWEST_CHIP,
+    INDIAN_FLAG,
+    SEAL_OF_TRUST,
+    USER_1,
+    USER_2,
+    USER_3,
+    BALL,
+    HAND,
+    CAR,
+    SPACE,
+    BIKE,
+    PLAYING_BUTTON,
+    ROBOTICS,
+    IoSearch,
+    BiFilterAlt,
+    IoIosTrendingUp, 
+    GoSponsorTiers, 
+    IoHeart,
+    IoCaretDownOutline
 }
